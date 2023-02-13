53ea62671b26b3dbb13f8d1f86c32a901d43621d
jianwen12390 <48422533+jianwen12390@users.noreply.github.com>
2023-02-13 17:41:08 +0800

    .

--- app/src/main/java/edu/singaporetech/services/GameGLSurfaceView.kt
+++ app/src/main/java/edu/singaporetech/services/GameGLSurfaceView.kt
@@ -1,18 +1,20 @@
-//import android.content.Context
-//import android.opengl.GLSurfaceView
-//
-//class GameGLSurfaceView(context: Context) : GLSurfaceView(context) {
-//
-//    private val renderer: GameGLRenderer
-//
-//    init {
-//
-//        // Create an OpenGL ES 2.0 context
-//        setEGLContextClientVersion(2)
-//
-//        renderer = GameGLRenderer()
-//
-//        // Set the Renderer for drawing on the GLSurfaceView
-//        setRenderer(renderer)
-//    }
-//}
+package edu.singaporetech.services
+import android.content.Context
+import android.opengl.GLSurfaceView
+
+
+class GameGLSurfaceView(context: Context) : GLSurfaceView(context) {
+
+    private val renderer: GameGLRenderer
+
+    init {
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2)
+
+        renderer = GameGLRenderer()
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(renderer)
+    }
+}
