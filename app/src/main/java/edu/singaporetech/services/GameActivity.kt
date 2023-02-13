062872978964dc8778ecd9900864bd9ee0caeb09
reuvenvgc <72001038+reuvenvgc@users.noreply.github.com>
2023-02-13 19:28:10 +0800

    psuh

--- app/src/main/java/edu/singaporetech/services/GameActivity.kt
+++ app/src/main/java/edu/singaporetech/services/GameActivity.kt
@@ -3,13 +3,16 @@ package edu.singaporetech.services
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class GameActivity : AppCompatActivity() {
 
     private val handler = Handler()
-    private val updateInterval = 16L // update interval in milliseconds
+    private val updateInterval = 16L // update interval in millisecond
+    private lateinit var fpsView: TextView
+    private lateinit var dtView: TextView
 
     private val updateRunnable = object : Runnable {
         var previousTime = System.currentTimeMillis()
@@ -17,16 +20,22 @@ class GameActivity : AppCompatActivity() {
         var deltaTime : Float = 0f
         var frames = 0
 
+
         override fun run() {
             // Perform tasks here when the activity is updated
             frames++
+
+            deltaTime = (System.currentTimeMillis() - previousTime) / 1000f
+            previousTime = System.currentTimeMillis()
+
             if (System.currentTimeMillis() - fpsTime >= 1000) {
-                Log.d("Game:", "Game is Running at $frames fps")
+                //Log.d("Game:", "Game is Running at $frames fps")
+                fpsView?.text = "$frames FPS"
+                dtView?.text = "${deltaTime}s dt"
                 frames = 0
                 fpsTime = System.currentTimeMillis()
             }
-            deltaTime = (System.currentTimeMillis() - previousTime) / 1000f
-            previousTime = System.currentTimeMillis()
+
             onUpdate(deltaTime)
             handler.postDelayed(this, updateInterval)
         }
@@ -35,14 +44,14 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-
         // TODO: GAME LOGIC HERE
-
+        fpsView = findViewById(R.id.textViewFPS)
+        dtView = findViewById(R.id.textViewDeltaTime)
     }
 
     private fun onUpdate(dt : Float) {
         // Perform tasks here when the activity is updated
-        Log.d("Game:", "Game is Running at ${dt.toBigDecimal()} seconds per frame")
+       // Log.d("Game:", "Game is Running at ${dt.toBigDecimal()} seconds per frame")
     }
 
     override fun onResume() {
