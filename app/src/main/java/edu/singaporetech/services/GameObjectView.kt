22c4db030ba0aff9dc87517451afbe47debc418b
jlys97 <72723142+jlys97@users.noreply.github.com>
2023-02-15 16:41:51 +0800

    draw player

--- app/src/main/java/edu/singaporetech/services/GameObjectView.kt
+++ app/src/main/java/edu/singaporetech/services/GameObjectView.kt
@@ -15,6 +15,7 @@ class GameObjectView(context: Context, attrs: AttributeSet?) : View(context, att
     private val screenHeight = displayMetrics.heightPixels
     private val paint = Paint()
     private val textureBitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.sit_logo)
+    private val imageView: GameGLSquare
 
     private var xPos: Float = 0f
     private var yPos: Float = 0f
@@ -23,10 +24,16 @@ class GameObjectView(context: Context, attrs: AttributeSet?) : View(context, att
     private val minYPos: Float = 50f
     private val maxYPos: Float = (screenHeight - 50f) // height is the height of the screen
 
+    init{
+        imageView = GameGLSquare(context)
+        imageView.setImageResource(R.drawable.coin)
+    }
     fun updatePosition(x: Float, y: Float) {
         xPos = max(minXPos, min(x, maxXPos))
         yPos = max(minYPos, min(y, maxYPos))
-        invalidate()
+        imageView.x = xPos
+        imageView.y = yPos
+        /*invalidate()*/
     }
     fun getXPosition():Float
     {
@@ -36,9 +43,9 @@ class GameObjectView(context: Context, attrs: AttributeSet?) : View(context, att
     {
         return yPos
     }
-    override fun onDraw(canvas: Canvas?) {
+   /* override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        /*canvas?.drawColor(Color.WHITE) // clear canvas to white
+        *//*canvas?.drawColor(Color.WHITE) // clear canvas to white
 
         // set up the paint object to use the texture bitmap
         val shader = BitmapShader(textureBitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT)
@@ -46,7 +53,7 @@ class GameObjectView(context: Context, attrs: AttributeSet?) : View(context, att
 
         // draw a rectangle with the texture
         val rect = RectF(0f, 0f, width.toFloat(), height.toFloat())
-        canvas?.drawRect(rect, paint)*/
+        canvas?.drawRect(rect, paint)*//*
         canvas?.drawCircle(xPos, yPos, 50f, Paint().apply { color = Color.RED })
-    }
+    }*/
 }
